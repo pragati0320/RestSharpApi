a83fc6e28fbbc855f90a87f0626a43671bd176e8
pragati0320 <pragati0320@gmail.com>
2022-10-16 19:41:35 +0545

    first api automation

---
+++ Calculator.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using RestSharp;
+
+namespace RestSharp_Api
+{
+    class Calculator
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+
+        public int Add()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
