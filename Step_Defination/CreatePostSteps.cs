8a0e2fe1d2ea9ec88e1651de9924915ab9cce96d
pragati0320 <pragati0320@gmail.com>
2022-10-18 12:33:22 +0545

    response verify of api

---
+++ Step_Defination/CreatePostSteps.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace RestSharp_Api.NewFolder
+{
+
+
+
+    namespace RestSharp.Demo.StepDefinition
+    {
+        [Binding]
+        public sealed class CreatePostSteps
+        {
+            private static CreatePosts posts;
+            public static string jsonBody;
+
+            public object SharedSteps { get; private set; }
+
+            [When(@"I create a request body with the following values")]
+            public void WhenICreateARequestBodyWithTheFollowingValues(Table table)
+            {
+                posts = table.CreateInstance<CreatePosts>();
+
+                var obj = new CreatePosts()
+                {
+                    userId = posts.userId,
+                    title = posts.title,
+                    body = posts.body
+                };
+
+                jsonBody = JsonConvert.SerializeObject(obj);
+                Console.WriteLine(jsonBody);
+            }
+
+            [When(@"Add the serialized body to the API request")]
+            public void WhenAddTheSerializedBodyToTheAPIRequest()
+            {
+                object p = SharedSteps.request.AddJsonBody(jsonBody);
+            }
+        }
+
+        internal class CreatePosts
+        {
+            internal object title;
+            internal object body;
+
+            public object userId { get; internal set; }
+        }
+    }
+}
--- Step_Defination/RestAPIHelper.cs
+++ Step_Defination/RestAPIHelper.cs
@@ -1,10 +1,38 @@
-﻿using System;
+﻿using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace RestSharp_Api.NewFolder
 {
-    class APIHelper
+    public static class APIHelper
     {
+
+        public static RestClient client;
+
+        public static RestRequest restRequest;
+
+        public static string baseUrl = "https://httpbin.org/get";
+
+        public static RestClient SetUrl(string endpoint)
+        {
+            var url = baseUrl + endpoint;
+            return client = new RestClient(url);
+        }
+
+        public static RestRequest CreateRequest()
+        {
+            restRequest = new RestRequest(Method.Get);
+
+            restRequest.AddHeader("Accept", "application/json");
+
+            return restRequest;
+        }
+
+        public static IRestResponse GetResponse()
+        {
+            return client.Execute(restRequest);
+        }
+
     }
 }
